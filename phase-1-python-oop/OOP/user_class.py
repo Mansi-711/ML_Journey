c98b033f04789ecb4a113892e3fc2d6f69139a0a
Mansi-711 <151171317+Mansi-711@users.noreply.github.com>
2026-04-14 09:49:44 +0530

    Create user_class.py

---
+++ phase-1-python-oop/OOP/user_class.py
@@ -0,0 +1,29 @@
+class User():
+  def __init__(self,first,last,mail,phn_num):
+    self.first_name = first
+    self.last_name = last
+    self.email = mail
+    self.phone_number = phn_num
+    self.login_attempt = 0
+
+  def describe_user(self):
+    print(f'The name of user is {self.first_name.title()} {self.last_name.title()} with e-mail address {self.email} and phone number {self.phone_number}')
+
+  def greet_user(self):
+    print(f'Hello {self.first_name.title()} {self.last_name.title()}')
+
+  def increment_login_attempts(self):
+    self.login_attempt += 1
+    print(f'The total login attempts are {self.login_attempt}')
+
+  def reset_login_attempt(self):
+    self.login_attempt = 0
+    print(f'The login attempts are reseted now {self.login_attempt}')
+
+u1 = User('Albert','einstein','albert@gmail.com',904352)
+u1.describe_user()
+u1.greet_user()
+u1.increment_login_attempts()
+u1.increment_login_attempts()
+u1.reset_login_attempt()
+
