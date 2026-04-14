59afde4ebda40a4cfed4fb5658428893df1de43a
Mansi-711 <151171317+Mansi-711@users.noreply.github.com>
2026-04-14 16:01:20 +0530

    Update user_class.py

--- phase-1-python-oop/OOP/user_class.py
+++ phase-1-python-oop/OOP/user_class.py
@@ -20,10 +20,23 @@ class User():
     self.login_attempt = 0
     print(f'The login attempts are reseted now {self.login_attempt}')
 
+class Admin(User):
+  def __init__(self, first, last, mail, phn_num):
+    super().__init__(first, last, mail, phn_num)
+    self.privilege = [ "can add post", "can delete post", "can ban user"]
+    
+  def show_privileges(self):
+      print('Your privileges are: ')
+    for i in self.privilege:
+      print(i)
+      
 u1 = User('Albert','einstein','albert@gmail.com',904352)
 u1.describe_user()
 u1.greet_user()
 u1.increment_login_attempts()
 u1.increment_login_attempts()
 u1.reset_login_attempt()
+
+a1 = Admin('Eric','Matthes', 'eric@gmail.com', 94335)
+a1.show_privileges()
 
