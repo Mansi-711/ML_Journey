70ec924a0c5a936377186b6352ceb0a4131650ee
Mansi-711 <151171317+Mansi-711@users.noreply.github.com>
2026-04-14 15:51:51 +0530

    Update restuarant_class.py

--- phase-1-python-oop/OOP/restuarant_class.py
+++ phase-1-python-oop/OOP/restuarant_class.py
@@ -3,7 +3,7 @@ class Restaurant():
         self.name = name
         self.cuisine = cuisine
         self.number_served = 0
-    def describe_restuarant(self):
+    def describe_restaurant(self):
         print(f'The name of the restaurant is {self.name} and it has {self.cuisine} cuisine.')
 
     def open_restaurant(self):
@@ -17,15 +17,27 @@ class Restaurant():
         self.number_served += increment
         print(f'{self.name} has served {self.number_served} customers')
 
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurant,cuisine,*flavors):
+        super().__init__(restaurant,cuisine)
+        self.flavors = list(flavors)
+    def display_flavors(self):
+        print('The flavors available are: ')
+        for i in self.flavors:
+            print(i)
+            
 r1 = Restaurant('Taj Mahal', 'Indian')
-r1.describe_restuarant()
+r1.describe_restaurant()
 r1.open_restaurant()
 r1.set_customer_served(300)
 r1.increment_customer_served(400)
 
 r2 = Restaurant('Osteria','Italian')
-r2.describe_restuarant()
+r2.describe_restaurant()
 r2.open_restaurant()
 r2.set_customer_served(400)
 r2.increment_customer_served(500)
+
+i1 = IceCreamStand('Amul', 'ice cream', 'vanilla','chacolate','pista','strawberry')
+i1.display_flavors()
 
